0606dd56454fb348a289e6ebfa4d4662a3e51848
Shraddha <shraddha@lob.com>
2018-05-28 20:22:21 -0700

    adding routes and responses

--- lib/firstelixir/handler.ex
+++ lib/firstelixir/handler.ex
@@ -19,18 +19,18 @@ defmodule Firstelixir.Handler do
     %{ mathod: method, path: path, resp_body: ""}
   end
 
-  def route(request) do
+  def route(conv) do
     # TODO: Create a new map that also has the response body
-    conv = %{ mathod: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers"}
+    %{ conv | resp_body: "Bears, Lions, Tigers"}
   end
 
   def format_response(conv) do
     """
     HTTP/1.1 200 OK
     Content-Type: text/html
-    Content-Length: 20
+    Content-Length: #{String.length(conv.resp_body)}
 
-    Bears, Lions, Tigers
+    #{conv.resp_body}
     """
   end
 end
