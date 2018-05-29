916a2602f83c3c1a50ab2e369cd5b71d55b039ab
Shraddha <shraddha@lob.com>
2018-05-29 09:54:09 -0700

    request params and status codes

--- lib/firstelixir/handler.ex
+++ lib/firstelixir/handler.ex
@@ -17,7 +17,11 @@ defmodule Firstelixir.Handler do
     |> String.split("\n")
     |> List.first
     |> String.split(" ")
-    %{ method: method, path: path, resp_body: ""}
+    %{ method: method,
+       path: path,
+       resp_body: "",
+       status: nil
+      }
   end
 
   def route(conv) do
@@ -25,21 +29,40 @@ defmodule Firstelixir.Handler do
   end
 
   def route(conv, "GET", "/wildthings") do
-    %{ conv | resp_body: "Bears, Lions, Tigers"}
+    %{ conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
 
   def route(conv, "GET", "/bears") do
-    %{ conv | resp_body: "Bear1, Bear2, Bear3"}
+    %{ conv | status: 200, resp_body: "Bear1, Bear2, Bear3"}
+  end
+
+  def route(conv, "GET", "/bears/" <> id) do
+    %{ conv | status: 200, resp_body: "Bear #{id}"}
+  end
+
+  def route(conv, _method, path) do
+    %{ conv | status: 404, resp_body: "No #{path} here!" }
   end
 
   def format_response(conv) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
     Content-Length: #{String.length(conv.resp_body)}
 
     #{conv.resp_body}
     """
+  end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
   end
 end
 
@@ -56,6 +79,27 @@ IO.puts response
 
 request = """
 GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+response = Firstelixir.Handler.handle(request)
+IO.puts response
+
+request = """
+GET /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+response = Firstelixir.Handler.handle(request)
+IO.puts response
+
+
+request = """
+GET /bigfoot HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
