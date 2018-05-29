9364a9317101f1a333bec02e24b61b1819e3345c
Shraddha <shraddha@lob.com>
2018-05-29 10:25:40 -0700

    Rewrite paths and track 404s

--- lib/firstelixir/handler.ex
+++ lib/firstelixir/handler.ex
@@ -2,9 +2,28 @@ defmodule Firstelixir.Handler do
   def handle(request) do
     request
     |> parse
+    |> rewrite_path
     |> log
     |> route
+    |> track
     |> format_response
+  end
+
+  def track(%{status: 404, path: path} = conv) do
+    IO.puts "Warning: #{path} is on the loose!"
+    conv
+  end
+
+  def track(conv) do
+    conv
+  end
+
+  def rewrite_path(%{ path: "/wildlife"} = conv) do
+    %{ conv | path: "/wildthings"}
+  end
+
+  def rewrite_path(conv) do
+    conv
   end
 
   def log(conv) do
@@ -12,7 +31,6 @@ defmodule Firstelixir.Handler do
   end
 
   def parse(request) do
-    # TODO: Parse the request string into key-value pair/ map
     [method, path, _] = request
     |> String.split("\n")
     |> List.first
@@ -24,23 +42,23 @@ defmodule Firstelixir.Handler do
       }
   end
 
-  def route(conv) do
-    route(conv, conv.method, conv.path)
-  end
+  # def route(conv) do
+  #   route(conv, conv.method, conv.path)
+  # end
 
-  def route(conv, "GET", "/wildthings") do
+  def route(%{ method: "GET", path: "/wildthings"} = conv) do
     %{ conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
 
-  def route(conv, "GET", "/bears") do
+  def route(%{ method: "GET", path: "/bears"} = conv) do
     %{ conv | status: 200, resp_body: "Bear1, Bear2, Bear3"}
   end
 
-  def route(conv, "GET", "/bears/" <> id) do
+  def route(%{ method: "GET", path: "/bears/"<>id} = conv) do
     %{ conv | status: 200, resp_body: "Bear #{id}"}
   end
 
-  def route(conv, _method, path) do
+  def route(%{path: path} = conv) do
     %{ conv | status: 404, resp_body: "No #{path} here!" }
   end
 
@@ -105,5 +123,17 @@ User-Agent: ExampleBrowser/1.0
 Accept: */*
 
 """
+response = Firstelixir.Handler.handle(request)
+IO.puts response
+
+
+request = """
+GET /wildlife HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
 response = Firstelixir.Handler.handle(request)
 IO.puts response
