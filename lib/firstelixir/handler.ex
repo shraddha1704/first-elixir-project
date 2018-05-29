dd017c73436d206694205a67ecd8f0bb331b32ff
Shraddha <shraddha@lob.com>
2018-05-29 09:41:05 -0700

    handling multiple routes, function clauses

--- lib/firstelixir/handler.ex
+++ lib/firstelixir/handler.ex
@@ -1,13 +1,14 @@
 defmodule Firstelixir.Handler do
   def handle(request) do
-    # conv = parse(request)
-    # conv = route(conv)
-    # format_response(conv)
-
     request
     |> parse
+    |> log
     |> route
     |> format_response
+  end
+
+  def log(conv) do
+    IO.inspect conv
   end
 
   def parse(request) do
@@ -16,12 +17,19 @@ defmodule Firstelixir.Handler do
     |> String.split("\n")
     |> List.first
     |> String.split(" ")
-    %{ mathod: method, path: path, resp_body: ""}
+    %{ method: method, path: path, resp_body: ""}
   end
 
   def route(conv) do
-    # TODO: Create a new map that also has the response body
+    route(conv, conv.method, conv.path)
+  end
+
+  def route(conv, "GET", "/wildthings") do
     %{ conv | resp_body: "Bears, Lions, Tigers"}
+  end
+
+  def route(conv, "GET", "/bears") do
+    %{ conv | resp_body: "Bear1, Bear2, Bear3"}
   end
 
   def format_response(conv) do
@@ -42,6 +50,16 @@ User-Agent: ExampleBrowser/1.0
 Accept: */*
 
 """
-response = Firstelixir.Handler.handle(request)
 
+response = Firstelixir.Handler.handle(request)
+IO.puts response
+
+request = """
+GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+response = Firstelixir.Handler.handle(request)
 IO.puts response
